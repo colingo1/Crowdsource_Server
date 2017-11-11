5d864d24048cb1e48f4231c5a2c1666f0da4896b
Marisa <leem19@rpi.edu>
2017-11-11 14:43:48 -0500

    test server connection

--- site/index.php
+++ site/index.php
@@ -1,4 +1,20 @@
 <html>
+
+<?php
+$servername = "localhost";
+$username = "user";
+$password = "GurQFa27qzH5VxaA";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
 working on this stuff
 I can edit.
 </html>
