c960136ae149fea7dc9b2596813744b5b632b46d
Marisa <leem19@rpi.edu>
2017-11-11 16:08:10 -0500

    try to insert into db

--- site/index.php
+++ site/index.php
@@ -4,15 +4,27 @@
 $servername = "localhost";
 $username = "user";
 $password = "GurQFa27qzH5VxaA";
+$dbname = "crowdsourcing"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO users (username,password) VALUES (?, ?)");
+$stmt->bind_param("sb", $username, $password);
+
+// set parameters and execute
+$username = "User1";
+$password = "Password1";
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
 ?>
 
 working on this stuff
