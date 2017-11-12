8273b179913d626b51ba24000f2fc4ba80f2d227
Marisa <leem19@rpi.edu>
2017-11-11 22:51:14 -0500

    authentification

--- site/index.php
+++ site/index.php
@@ -1,9 +1,9 @@
 <html>
 
 <?php
-$servername = "localhost";
+$servername = "129.161.69.63";
 $username = "user";
-$password = "GurQFa27qzH5VxaA";
+$password = "";
 $dbname = "crowdsourcing";
 
 // Create connection
@@ -14,32 +14,61 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// prepare and bind
-$stmt = $conn->prepare("INSERT INTO users (username,password) VALUES (?, ?)");
-$stmt->bind_param("sb", $username, $password);
+$data = json_decode($_POST["json"]);
+// var_dump($data);
+// echo $data->action;
+if($data->action == "create")
+{
+	// prepare and bind
+	$stmt = $conn->prepare("INSERT INTO users (username,password,first_name,last_name) VALUES (?, ?, ?, ?)");
+	$stmt->bind_param("ssss", $user, $pass, $first, $last);
 
-// set parameters and execute
-$username = "User1";
-$password = "Password1";
-$stmt->execute();
+	// set parameters and execute
+	$user = $data->username;
+	$pass = $data->password;
+	$first = $data->first;
+	$last = $data->last;
+	if(!$stmt->execute())
+	{
 
-$stmt->close();
-
-
-$sql = "SELECT * FROM users";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "username " . $row["username"]. " - Password: " . $row["password"]. "<br>";
-    }
-} else {
-    echo "0 results";
+		$result = "This user already exists";
+		$json = json_encode($result);
+		echo($json);
+	}
+	$stmt->close();
 }
+else
+{
+	// prepare and bind
+	$stmt = $conn->prepare("SELECT password FROM users WHERE username=?");
+	// echo($conn->error);
+	$stmt->bind_param("s", $user);
 
+	// set parameters and execute
+	$user = $data->username;
+	if(!$stmt->execute())
+	{
+		$result = "Try Again.";
+		$json = json_encode($result);
+		echo $json;
+	}
 
+    $stmt->bind_result($dbpassword);
+    $stmt->fetch();
+    if($dbpassword != $data->password)
+    {    	
+		$result = "Try Again.";
+		$json = json_encode($result);
+		echo $json;
+    }
+    else
+    {
+	    session_start();
+	    $json = json_encode(session_id());
+	    echo $json;
+    }
+	$stmt->close();
+}
 
 $conn->close();
 ?>
