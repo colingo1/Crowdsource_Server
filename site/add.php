281a502edbf162b03054acb56c42755949050682
colingo1 <colin.goldberg@asdnh.org>
2017-11-11 20:44:10 -0500

    finished add.php

---
+++ site/add.php
@@ -0,0 +1,33 @@
+
+<?php
+session_start();
+$servername = "129.161.69.63";
+$db_username = "user";
+$db_password = "";
+$dbname = "crowdsourcing";
+//if($_SESSION["logged_in"]=true)
+//{
+	// Create connection
+	$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	// prepare and bind
+	$question = json_decode($_POST["json"]);
+	print_r($question);
+	$stmt = $conn->prepare("INSERT INTO questions (question,asker,location,tags,accepted_answer,rating) VALUES (?,?,?,?,0,?)");
+	$stmt->bind_param("ssssi", $question->title, $testing, $question->location,$question->tags,$question->rating);
+	$testing = "something";
+	// set parameters and execute
+	$stmt->execute();
+
+	$stmt->close();
+
+
+	$conn->close();
+
+//}
+?>
